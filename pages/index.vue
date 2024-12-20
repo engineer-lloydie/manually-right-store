89065f894545d71b13663c675097f8dc0352e385
Lloydie <lloydie.glophics@gmail.com>
2024-12-20 16:58:37 +0800

    Updates

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>{{ testTitle }}</h1>
+
+    </div>
+</template>
+
+<script setup>
+    const testTitle = 'Hello World';
+</script>
+
+<style lang="scss" scoped>
+
+</style>
