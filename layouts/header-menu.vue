1ae0c6bcccc4f811acf3e1c53626f56a560def83
Lloydie <lloydie.glophics@gmail.com>
2025-01-15 13:44:22 +0800

    Updates

--- layouts/header-menu.vue
+++ layouts/header-menu.vue
@@ -1,22 +1,33 @@
 <template>
     <v-layout>
-        <v-app-bar max-width="500" class="px-md-4">
-            <template #prepend>
-                <v-app-bar-nav-icon v-if="$vuetify.display.smAndDown" @click="drawer = !drawer" />
-            </template>
-
-            <v-img class="me-sm-8" max-width="100" src="~/assets/images/partial-logo.png" />
-
-            <v-spacer />
-
-            <template #append>
-                <v-btn class="ms-1" color="medium-emphasis" icon="mdi-account" />
-                <v-btn class="ms-1" color="medium-emphasis" icon="mdi-cart" />
-            </template>
+        <v-app-bar>
+            <v-container>
+                <v-row align="center">
+                    <v-col cols="4">
+                        <v-img class="me-sm-8" max-width="100" src="~/assets/images/partial-logo.png" />
+                    </v-col>
+                    <v-col cols="8">
+                        <div class="d-flex justify-end">
+                            <v-btn class="ms-1" color="medium-emphasis" icon="mdi-magnify" />
+                            <v-btn class="ms-1" color="medium-emphasis" icon="mdi-cart" />
+                            <v-btn class="ms-1" color="medium-emphasis" icon="mdi-account" />
+                        </div>
+                    </v-col>
+                </v-row>
+            </v-container>
         </v-app-bar>
 
         <v-main height="200">
-            <!--  -->
+            <v-tabs
+                v-model="tab"
+                align-tabs="end"
+                bg-color="grey-darken-3"
+                color="yellow-lighten-1"
+            >
+                <v-tab value="one">Item One</v-tab>
+                <v-tab value="two">Item Two</v-tab>
+                <v-tab value="three">Item Three</v-tab>
+            </v-tabs>
         </v-main>
     </v-layout>
 </template>
@@ -25,6 +36,7 @@ import { shallowRef } from 'vue'
 
 const appTitle = ref("Upright Manual");
 const sidebar = ref(false);
+const tab = ref("one");
 
 const menuItems = ref([
     { title: "Home", path: "/home", icon: "mdi-home" },
