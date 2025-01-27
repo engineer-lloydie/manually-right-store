6372918c27343401cb8666b7437b3ccc4b448c49
Lloydie <lloydie.glophics@gmail.com>
2025-01-27 14:48:56 +0800

    New Updates

--- pages/documents/categories/[id].vue
+++ pages/documents/categories/[id].vue
@@ -13,8 +13,8 @@
                         sm="12"
                         xl="3"
                         class="d-flex justify-sm-center"
-                        v-for="n in 8"
-                        :key="n"
+                        v-for="item in manualItems"
+                        :key="item.id"
                     >
                         <v-card
                             class="ma-4"
@@ -27,14 +27,14 @@
                             >
                             </v-img>
 
-                            <v-card-title>Air & Space Model 18A Gyroplane Maintenance & Rigging Manual 1965 (Report No. UER 18-601)</v-card-title>
+                            <v-card-title>{{ item.name }}</v-card-title>
 
-                            <v-card-subtitle class="text-h6 font-weight-bold" color="red">
-                                $100.00
+                            <v-card-subtitle class="text-h6 font-weight-bold">
+                                ${{ item.price }}.00
                             </v-card-subtitle>
 
                             <v-card-text>
-                                <div>Air & Space Model 18A Gyroplane Maintenance & Rigging Manual 1965 (Report No. UER 18-601)</div>
+                                <div>{{ item.description }}</div>
                             </v-card-text>
 
                             <v-card-actions>
@@ -61,7 +61,7 @@
                 <div>
                     <v-pagination
                       v-model="page"
-                      :length="6"
+                      :length="pageCount"
                       :total-visible="5"
                     ></v-pagination>
                   </div>
@@ -72,6 +72,60 @@
 
 <script setup>
 const page = ref(1);
+const manualItems = ref([
+    {
+        id: 1,
+        name: 'Air & Space Model 18A Gyroplane Maintenance & Rigging Manual 1965 (Report No. UER 18-601)',
+        description: "Air & Space Model 18A Gyroplane Maintenance & Rigging Manual 1965 (Report No. UER 18-601)",
+        price: 100
+    },
+    {
+        id: 2,
+        name: 'Air & Space Model 18A Gyroplane Maintenance & Rigging Manual 1965 (Report No. UER 18-601)',
+        description: "Air & Space Model 18A Gyroplane Maintenance & Rigging Manual 1965 (Report No. UER 18-601)",
+        price: 100
+    },
+    {
+        id: 3,
+        name: 'Air & Space Model 18A Gyroplane Maintenance & Rigging Manual 1965 (Report No. UER 18-601)',
+        description: "Air & Space Model 18A Gyroplane Maintenance & Rigging Manual 1965 (Report No. UER 18-601)",
+        price: 100
+    },
+    {
+        id: 4,
+        name: 'Air & Space Model 18A Gyroplane Maintenance & Rigging Manual 1965 (Report No. UER 18-601)',
+        description: "Air & Space Model 18A Gyroplane Maintenance & Rigging Manual 1965 (Report No. UER 18-601)",
+        price: 100
+    },
+    {
+        id: 5,
+        name: 'Air & Space Model 18A Gyroplane Maintenance & Rigging Manual 1965 (Report No. UER 18-601)',
+        description: "Air & Space Model 18A Gyroplane Maintenance & Rigging Manual 1965 (Report No. UER 18-601)",
+        price: 100
+    },
+    {
+        id: 6,
+        name: 'Air & Space Model 18A Gyroplane Maintenance & Rigging Manual 1965 (Report No. UER 18-601)',
+        description: "Air & Space Model 18A Gyroplane Maintenance & Rigging Manual 1965 (Report No. UER 18-601)",
+        price: 100
+    },
+    {
+        id: 8,
+        name: 'Air & Space Model 18A Gyroplane Maintenance & Rigging Manual 1965 (Report No. UER 18-601)',
+        description: "Air & Space Model 18A Gyroplane Maintenance & Rigging Manual 1965 (Report No. UER 18-601)",
+        price: 100
+    },
+    {
+        id: 8,
+        name: 'Air & Space Model 18A Gyroplane Maintenance & Rigging Manual 1965 (Report No. UER 18-601)',
+        description: "Air & Space Model 18A Gyroplane Maintenance & Rigging Manual 1965 (Report No. UER 18-601)",
+        price: 100
+    }
+]);
+
+const pageCount = computed(() => {
+    return Math.ceil(manualItems.value.length / 8);
+});
 </script>
 
 <style lang="scss" scoped>
