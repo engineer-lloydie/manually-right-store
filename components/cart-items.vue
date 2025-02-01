53d9aa3457212df8356eff6b17ddb7b84deae287
lloydie <lloydie.glophics@gmail.com>
2025-02-01 16:08:15 +0800

    Updates

--- components/cart-items.vue
+++ components/cart-items.vue
@@ -1,6 +1,6 @@
 <template>
 	<div>
-		<v-menu v-model="menu" :close-on-content-click="false" transition="scale-transition" scroll-strategy="close">
+		<v-menu v-model="menu" :close-on-content-click="false" transition="scale-transition" persistent :max-width="900" :max-height="600">
 			<template v-slot:activator="{ props }">
 				<v-btn
                     color="grey-darken-1"
@@ -29,23 +29,24 @@
 				<v-card-item>
                     <v-card-text>
                         <v-sheet class="mb-5">
-                            <v-row v-for="item in cartItems" :key="item.id" align="center">
-                                <v-col cols="3">
+                            <v-row v-for="(item, index) in cartItems" :key="item.id" align="center">
+                                <v-col cols="12" sm="3" md="3" lg="3" xl="3">
                                     <v-img 
                                         src="~/assets/images/thumbnail.jpeg"
                                         max-height="100"
                                     />
                                 </v-col>
-                                <v-col cols="5">
+                                <v-col cols="12" sm="5" md="5" lg="5" xl="5">
                                     <h5 class="mb-1">{{ item.name }}</h5>
                                     <h5 class="font-weight-regular">Quantity: {{ item.quantity }}</h5>
                                 </v-col>
-                                <v-col cols="3">
-                                    <h4 class="price-text-color text-right">${{ item.price }}.00</h4>
+                                <v-col cols="12" sm="2" md="2" lg="2" xl="2">
+                                    <h4 class="price-text-color text-center">${{ item.price }}.00</h4>
                                 </v-col>
-                                <v-col cols="12" sm="12" md="1" lg="1" xl="1" class="text-center">
+                                <v-col cols="12" sm="2" md="2" lg="2" xl="2" class="text-center">
                                     <v-btn icon="mdi-delete" size="30" color="red-lighten-1" variant="text"></v-btn>
                                 </v-col>
+                                <v-divider inset v-if="(index + 1) != cartItems.length"></v-divider>
                             </v-row>
                         </v-sheet>
                         <v-sheet color="grey-lighten-3 pa-5 mb-5">
