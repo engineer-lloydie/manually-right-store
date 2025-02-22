e1d1938a997face0e0a21cb3faeaa092eb88131d
lloydie <lloydie.glophics@gmail.com>
2025-02-22 15:18:51 +0800

    Updates

--- components/suggested-products.vue
+++ components/suggested-products.vue
@@ -8,62 +8,77 @@
             elevation="1"
             max-width="1200"
         >
-            <v-slide-group
-                class="pa-4"
-                center-active
-                show-arrows
-                :mobile="false"
-            >
-                <v-slide-group-item
-                    v-for="item in manualItems"
-                    :key="item.id"
-                >
-                    <v-card
-                        class="ma-4"
-                        width="400"
+            <v-sheet class="d-flex justify-center align-center" height="400" v-if="processing">
+                <v-progress-circular color="red-lighten-1" indeterminate></v-progress-circular>
+            </v-sheet>
+            <template v-else>
+                <template v-if="manualItems.length">
+                    <v-slide-group
+                        class="pa-4"
+                        center-active
+                        show-arrows
+                        :mobile="false"
                     >
-                        <v-img
-                            class="align-end text-white"
-                            height="320"
-                            src="~/assets/images/thumbnail.jpeg"
+                        <v-slide-group-item
+                            v-for="item in manualItems"
+                            :key="item.id"
                         >
-                        </v-img>
-
-                        <v-card-title>{{ item.name }}</v-card-title>
-
-                        <v-card-subtitle class="text-h6 font-weight-bold">
-                            ${{ item.price }}.00
-                        </v-card-subtitle>
-
-                        <v-card-text>
-                            <div>{{ item.description }}</div>
-                        </v-card-text>
-
-                        <v-card-actions class="d-flex justify-end">
-                            <v-btn
-                                :to="`/manuals/categories/${item.category_slug}/${item.url_slug}`"
-                                color="white" 
-                                text="View Details"
-                                prepend-icon="mdi-text-box-search-outline"
-                                elevation="2"
-                                class="bg-grey-darken-3"
-                            ></v-btn>
-                            <v-btn 
-                                color="white" 
-                                text="Add to cart"
-                                prepend-icon="mdi-cart-check"
-                                elevation="2"
-                                class="bg-red-lighten-1"
-                            ></v-btn>
-                        </v-card-actions>
-                    </v-card>
-                </v-slide-group-item>
-            </v-slide-group>
+                            <v-card
+                                class="ma-4"
+                                width="400"
+                            >
+                                <v-img
+                                    class="align-end text-white"
+                                    height="320"
+                                    :src="item.thumbnails[0].url"
+                                >
+                                </v-img>
+        
+                                <v-card-title>{{ item.title }}</v-card-title>
+        
+                                <v-card-subtitle class="text-h6 font-weight-bold">
+                                    ${{ item.price }}.00
+                                </v-card-subtitle>
+        
+                                <v-card-text>
+                                    <div>{{ item.description }}</div>
+                                </v-card-text>
+        
+                                <v-card-actions class="d-flex justify-end">
+                                    <v-btn
+                                        :to="`/manuals/categories/${item.main_url_slug}/${item.sub_url_slug}/${item.url_slug}`"
+                                        color="white" 
+                                        text="View Details"
+                                        prepend-icon="mdi-text-box-search-outline"
+                                        elevation="2"
+                                        class="bg-grey-darken-3"
+                                    ></v-btn>
+                                    <v-btn 
+                                        color="white" 
+                                        text="Add to cart"
+                                        prepend-icon="mdi-cart-check"
+                                        elevation="2"
+                                        class="bg-red-lighten-1"
+                                        :loading="cartStore.addingCart"
+                                        @click="addCart(item)"
+                                    ></v-btn>
+                                </v-card-actions>
+                            </v-card>
+                        </v-slide-group-item>
+                    </v-slide-group>
+                </template>
+                <template v-else>
+                    <v-sheet class="d-flex justify-center align-center" height="400">No records found</v-sheet>
+                </template>
+            </template>
         </v-sheet>
     </div>
 </template>
 
 <script setup>
+import { useCartStore } from '@/store/cart';
+const cartStore = useCartStore();
+const { isAuthenticated, user } = useSanctumAuth();
 
 const props = defineProps({
     title: {
@@ -77,10 +92,44 @@ const props = defineProps({
     manualItems: {
         type: Array,
         required: true
+    },
+    processing: {
+        type: Boolean,
+        required: true
     }
 });
+
+const addCart = async(manual) => {
+    try {
+        cartStore.setNewAddedCart(false);
+        await cartStore.addToCart({
+            userId: isAuthenticated.value ? user.value.id : null,
+            guestId: isAuthenticated.value ? null : localStorage.getItem('guestId'),
+            manualId: manual.id,
+            price: manual.price,
+            quantity: 1
+        });
+
+        const params = {};
+        
+        if (isAuthenticated.value) {
+            params.userId = user.value.id;
+        } else {
+            params.guestId = localStorage.getItem('guestId')
+        }
+
+        await cartStore.fetchCartItems(params);
+        cartStore.setNewAddedCart(true);
+    } catch (error) {
+        console.error(error);
+    }
+}
 </script>
 
 <style lang="scss" scoped>
-
+.loader-center {
+    position: absolute;
+    top: 50;
+    left: 50;
+}
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -4,15 +4,53 @@
             <banner/>
         </div>
         <div class="my-15">
-            <suggested-products title="Our Best Selling" flag="best_selling" :manual-items="manualItems"/>
+            <suggested-products title="Our Best Selling" flag="best_selling" :processing="fetchingBestSelling" :manual-items="bestSelling"/>
         </div>
         <div class="my-15">
-            <suggested-products title="Our Latest Products" flag="latest_products" :manual-items="manualItems"/>
+            <suggested-products title="Our Latest Products" flag="latest_products" :processing="fetchingLatestProducts" :manual-items="latestProducts"/>
         </div>
     </div>
 </template>
 
 <script setup>
+const bestSelling = ref([]);
+const latestProducts = ref([]);
+const fetchingBestSelling = ref(false);
+const fetchingLatestProducts = ref(false);
+
+const getBestSelling = async () => {
+    try {
+        fetchingBestSelling.value = true;
+
+        const { data } = await useBaseFetch('/items/best-selling');
+
+        bestSelling.value = data;
+    } catch (error) {
+        console.error(error)
+    } finally {
+        fetchingBestSelling.value = false;
+    }
+}
+
+const getLatestProducts = async () => {
+    try {
+        fetchingLatestProducts.value = true;
+
+        const { data } = await useBaseFetch('/items/latest');
+
+        latestProducts.value = data;
+    } catch (error) {
+        console.error(error)
+    } finally {
+        fetchingLatestProducts.value = false;
+    }
+}
+
+onMounted(() => {
+    getBestSelling();
+    getLatestProducts();
+})
+
 const manualItems = ref([
     {
         id: 1,
