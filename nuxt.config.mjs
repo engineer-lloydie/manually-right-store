b7d3151f7020121272dc1101f68123f3427e024a
lloydie <lloydie.glophics@gmail.com>
2025-02-25 20:57:40 +0800

    Updates

--- nuxt.config.mjs
+++ nuxt.config.mjs
@@ -1,15 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 
-import dotenv from 'dotenv';
-import path from 'path';
-import fs from 'fs';
-
-const env = process.env.ENV || 'local';
-const envPath = path.resolve(process.cwd(), `.env.${env}`);
-const defaultEnvPath = path.resolve(process.cwd(), '.env');
-dotenv.config({ path: fs.existsSync(envPath) ? envPath : defaultEnvPath });
-
 export default defineNuxtConfig({
     compatibilityDate: '2024-11-11',
     devtools: { enabled: false },
