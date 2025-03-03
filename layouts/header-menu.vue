29a54754189a1e4394391568575e8ea65838b4e4
lloydie <lloydie.glophics@gmail.com>
2025-03-03 22:25:38 +0800

    Global Search Updates

--- layouts/header-menu.vue
+++ layouts/header-menu.vue
@@ -13,18 +13,96 @@
                     </v-col>
                     <v-col cols="12" sm="12" md="8" class="d-flex justify-center justify-md-end align-center">
                         <div class="d-flex align-center">
-                            <v-text-field
-                                :loading="searching"
-                                append-inner-icon="mdi-magnify"
-                                density="compact"
-                                label="Search templates"
-                                variant="solo"
-                                hide-details
-                                single-line
-                                width="300"
-                                @click:append-inner="search"
-                                class="global-search"
-                            ></v-text-field>
+                            <!-- Search Result items -->
+                            <v-menu
+                                v-model="menu"
+                                :close-on-content-click="false"
+                                close-on-back
+                                :open-on-focus="true"
+                                transition="scale-transition"
+                                :max-width="700" 
+                                width="700" 
+                                :max-height="600"
+                            >
+                                <template v-slot:activator="{ props }">
+                                    <v-text-field
+                                        density="compact"
+                                        label="Search templates"
+                                        variant="solo"
+                                        hide-details
+                                        single-line
+                                        width="300"
+                                        v-bind="props"
+                                        class="global-search"
+                                        v-model="searchQuery"
+                                        @input="search"
+                                    ></v-text-field>
+                                </template>
+                                <v-card min-width="300" title="Search Results">
+                                    <template v-slot:append>
+                                        <v-btn
+                                            density="comfortable"
+                                            icon="$close"
+                                            variant="plain"
+                                            @click="menu = !menu"
+                                        ></v-btn>
+                                    </template>
+                                    <v-card-item>
+                                        <v-card-text>
+                                            <template v-if="searching">
+                                                <v-skeleton-loader
+                                                    :loading="searching"
+                                                    type="list-item-avatar-two-line"
+                                                ></v-skeleton-loader>
+                                            </template>
+                                            <template v-else>
+                                                <template v-if="searchResult.length">
+                                                    <v-sheet
+                                                        class="cursor-pointer"
+                                                        v-for="(item, index) in searchResult"
+                                                        :key="item.id" 
+                                                        @click="navigateTo(`/manuals/categories/${item.main_url_slug}/${item.sub_url_slug}/${item.url_slug}`)"
+                                                    >
+                                                        <v-hover
+                                                            v-slot="{ isHovering, props }"
+                                                            open-delay="200"
+                                                        >
+                                                            <v-row align="center" class="search-result-row" :class="{ 'on-hover': isHovering }" v-bind="props">
+                                                                <v-col class="d-flex justify-between d-sm-inline" cols="12" sm="2">
+                                                                    <v-img 
+                                                                        :src="item.thumbnail_url"
+                                                                        lazy-src="~/assets/images/image-icon.png"
+                                                                        width="70"
+                                                                        height="70"
+                                                                        rounded
+                                                                    >
+                                                                        <template v-slot:placeholder>
+                                                                            <div class="d-flex align-center justify-center fill-height">
+                                                                                <v-progress-circular
+                                                                                    color="grey-lighten-4"
+                                                                                    indeterminate
+                                                                                ></v-progress-circular>
+                                                                            </div>
+                                                                        </template>
+                                                                    </v-img>
+                                                                </v-col>
+                                                                <v-col class="text-center text-sm-left" cols="12" sm="8">
+                                                                    <h5 class="mb-1">{{ item.title }}</h5>
+                                                                </v-col>
+                                                                <v-col cols="12" sm="2">
+                                                                    <h4 class="price-text-color text-center">${{ item.price }}</h4>
+                                                                </v-col>
+                                                                <v-divider v-if="(index + 1) != searchResult.length"></v-divider>
+                                                            </v-row>
+                                                        </v-hover>
+                                                    </v-sheet>
+                                                </template>
+                                                <p v-else class="text-center">No records found</p>
+                                            </template>
+                                        </v-card-text>
+                                    </v-card-item>
+                                </v-card>
+                            </v-menu>
                             
                             <cart-items class="mx-2"/>
 
@@ -92,9 +170,40 @@ const { isAuthenticated } = useSanctumAuth();
 const { $showModal, $logout } = useNuxtApp()
 
 const searching = ref(false);
-const search = () => {
-    searching.value = true;
-};
+const menu = ref(false);
+const showAlert = ref(false);
+const alertMessage = ref(null);
+const searchResult = ref([]);
+const searchQuery = ref('');
+
+const debounce = (func, timeout = 500) => {
+    let timer;
+    return (...args) => {
+        clearTimeout(timer);
+        timer = setTimeout(() => { func.apply(this, args); }, timeout);
+    };
+}
+
+const search = debounce(async () => {
+    try {
+        menu.value = true;
+        searching.value = true;
+
+        const { data } = await useBaseFetch('/items/search', {
+            method: 'GET',
+            params: {
+                search: searchQuery.value
+            }
+        });
+
+        searchResult.value = data;
+    } catch (error) {
+        console.error(error);
+    } finally {
+        searching.value = false;
+    }
+});
+
 const logoutDialog = ref(false);
 
 const handleDialog = () => {
@@ -119,7 +228,10 @@ const logoutUser = (async () => {
 @media screen and (max-width: 430px) {
     .global-search {
         width: 60vw !important;
-    }
-    
+    } 
+}
+
+.search-result-row:hover {
+    background-color: #F5F5F5;
 }
 </style>
